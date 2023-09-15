62cfd4f48b55107d5949282945f7f9092d0c40e0
tqt0304 <tqthy034@gmail.com>
2023-09-15 19:16:49 +0700

    Everything

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = express();
 // so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-
+app.set('trust proxy', true);
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -23,6 +23,11 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/whoami', (req, res) => {
+  // res.json({ipaddress: req.socket.remoteAddress});
+  res.json({ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
